119679ab53cce319bb0949763b248ff2c0906c35
nghia <nghiaplps17855@fpt.edu.vn>
2022-08-14 02:00:46 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const socketio = require('socket.io')
+const express = require('express')
+const http = require('http')
+const cors = require('cors')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+app.use(cors())
+
+let usersOnline = []
+
+const addUserOnline = (userId, socketId) => {
+    !usersOnline.some(user => user.userId === userId) &&
+        usersOnline.push({userId, socketId})
+}
+
+const removeUserOnline = (socketId) => {
+    usersOnline = usersOnline.filter(user => user.socketId !== socketId)
+}
+
+const getUser = (userId) => {
+    return usersOnline.find(user => user.userId === userId)
+}
+
+io.on("connection", (socket) => {
+
+    socket.on("addUserOnline", (userId) => {
+        addUserOnline(userId, socket.id)
+        io.emit("getUsersOnline", usersOnline)
+    })
+
+    socket.on("sendMessage", ({senderId, receiverId, text, createAt}) => {
+        const user = getUser(receiverId)
+        io.to(user.socketId).emit("getMessage" , {
+            senderId,
+            text,
+            createAt
+        })
+    })
+
+    socket.on("disconnect", () => {
+        removeUserOnline(socket.id)
+        io.emit("getUsersOnline", usersOnline)
+    })
+})
